7ae70b747e694aeb527b54b99e05f993b590c518
riyakumari29 <114248355+riyakumari29@users.noreply.github.com>
2022-09-24 22:07:49 +0530

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,19 @@
+package com.inh.pll;
+
+import com.inh.bll.Author;
+
+public class TestAuthor {
+
+		public static void main(String[] args) {
+			//Display Author's Detail's
+			Author author=new Author("Robert Kiyosaki ","robert123@gmail.com");
+			System.out.println("\n");
+	        System.out.println("**********************************************************************************************");
+	        System.out.println(author);
+	        System.out.println("**********************************************************************************************");
+
+		}
+
+
+	}
+
